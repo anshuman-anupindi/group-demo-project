f392bdc4251b517aac9d4c1c7d81b4b8d79b7701
Anshuman Anupindi <anshuman.anupindi@gmail.com>
2021-06-28 15:39:52 +1000

    hi world

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 4567
 
-app.get('/', (req, res) => res.send('hello world'))
+app.get('/', (req, res) => res.send('hi world'))
 
 app.listen(port, () => console.log(`listening on port: ${port}`))
